a749e782d5bc257f776e7b169868f0688c1ca2db
Kunal Shinde <kunalshinde373@gmail.com>
2020-05-20 03:14:13 +0530

    Updates

---
+++ precache-manifest.788c78239fcaa9cedee33f5b5ae2a78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20865751a19ad10e8fae4e383c6eec75",
+    "url": "/pro-organiser-application/index.html"
+  },
+  {
+    "revision": "30e204cb83553b3d6d19",
+    "url": "/pro-organiser-application/static/css/main.d556480d.chunk.css"
+  },
+  {
+    "revision": "04166d85d762590ea778",
+    "url": "/pro-organiser-application/static/js/2.ddb8631e.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/pro-organiser-application/static/js/2.ddb8631e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e204cb83553b3d6d19",
+    "url": "/pro-organiser-application/static/js/main.3e1fafb1.chunk.js"
+  },
+  {
+    "revision": "484817955d2fe2d0cd83",
+    "url": "/pro-organiser-application/static/js/runtime-main.85dec5da.js"
+  }
+]);
